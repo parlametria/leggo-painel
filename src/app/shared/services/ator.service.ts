0554b34cdefce0235d5736f713e6840e21c42bc6
gileadekelvin <gileadekelvin@gmail.com>
2020-06-05 13:42:25 -0300

    Resolve problema de build no Repositório

--- src/app/shared/services/ator.service.ts
+++ src/app/shared/services/ator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Ator } from '../models/ator.model';
