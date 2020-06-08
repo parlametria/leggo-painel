36567d2d2a894ec1c43ae125380f57d5b62d216a
gileadekelvin <gileadekelvin@gmail.com>
2020-06-08 18:33:22 -0300

    Adiciona funções para recuperar informações agregadas de atores e autorias Adiciona Forkjoin para agrupar chamadas para endpoints agregados de atividade parlamentar

--- src/app/atividade-parlamentar/atividade-parlamentar-routing.module.ts
+++ src/app/atividade-parlamentar/atividade-parlamentar-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AtividadeParlamentarComponent } from './atividade-parlamentar.component';
 
 const routes: Routes = [
-  { 
+  {
     path: '',
     component: AtividadeParlamentarComponent }
 ];
