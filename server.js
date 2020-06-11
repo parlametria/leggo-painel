db1465625f498fdf074bd599a1988b496ffeceae
Jefferson Neves <jefferson.rpn@gmail.com>
2020-06-11 15:13:55 -0300

    Adiciona typescript como dependência para permitir build no heroku

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const express = require("express")
+const serveStatic = require("serve-static")
+const path = require("path")
 
 const app = express()
 
-app.use(serveStatic('dist/leggo-painel'))
+app.use(serveStatic("dist/leggo-painel"))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'legoo-painel', 'index.html'));
+  res.sendFile(path.resolve(__dirname, "dist", "legoo-painel", "index.html"));
 });
 
 const port = process.env.PORT || 8080
