dbd21235e95ff16d6f24666ab7abd27e9549fe08
Luiz Augusto Morais <luizaugustomm@gmail.com>
2020-06-17 12:23:57 -0300

    Adiciona esboço inicial da tela de detalhes do parlamentar

--- src/app/atividade-parlamentar/detalhes-parlamentar/detalhes-parlamentar.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/detalhes-parlamentar.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhesParlamentarComponent implements OnInit {
 
+  urlFoto: string;
+  nome_autor: string;
+  partido: string;
+  uf: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    const id_autor = 107283;
+    this.urlFoto = `https://www.camara.leg.br/internet/deputado/bandep/${id_autor}.jpg`;
+    this.nome_autor = 'Dep. Gleisi Hoffmann';
+    this.partido = 'PT';
+    this.uf = 'PR';
   }
 
 }
