a3a2521d948767c032acad8e8cb4bf730e08b45e
Joao Menezes <joao.menezes@ccc.ufcg.edu.br>
2020-07-09 16:56:40 -0300

    novo design para o gráfico de ações

--- src/app/atividade-parlamentar/detalhes-parlamentar/vis-atividade-parlamentar/vis-atividade-parlamentar.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/vis-atividade-parlamentar/vis-atividade-parlamentar.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
 
 import { AtorService } from 'src/app/shared/services/ator.service';
-import { Autoria } from 'src/app/shared/models/autoria.model';
 
 // Importa componentes do d3
 import { select, selectAll } from 'd3-selection';
-import { scaleLinear, scaleBand, scaleLog } from 'd3-scale';
+import { scaleLinear, scaleBand, scaleOrdinal } from 'd3-scale';
 import { group } from 'd3-array';
 import { axisLeft, axisBottom } from 'd3-axis';
 
@@ -16,6 +16,7 @@ const d3 = Object.assign({}, {
     selectAll,
     scaleLinear,
     scaleBand,
+    scaleOrdinal,
     group,
     axisLeft,
     axisBottom
@@ -29,10 +30,10 @@ const d3 = Object.assign({}, {
 
 export class VisAtividadeParlamentarComponent implements OnInit {
 
-    @Input() idAtor: string;
-
     private unsubscribe = new Subject();
 
+    idAtor: number;
+    interesse: string;
     private largura: number;
     private altura: number;
     private gPrincipal: any;
@@ -42,12 +43,18 @@ export class VisAtividadeParlamentarComponent implements OnInit {
     private svg: any;
 
     constructor(
-        private atorService: AtorService
+        private atorService: AtorService, private activatedRoute: ActivatedRoute
     ) { }
 
     ngOnInit(): void {
+        this.activatedRoute.paramMap
+            .pipe(takeUntil(this.unsubscribe))
+            .subscribe(params => {
+                this.idAtor = parseInt(params.get('id'), 10);
+                this.interesse = params.get('interesse');
+        });
         this.largura = window.innerWidth / 2;
-        this.altura = 190;
+        this.altura = 140;
         this.margin = ({
             top: 20,
             right: 70,
@@ -63,70 +70,67 @@ export class VisAtividadeParlamentarComponent implements OnInit {
         this.carregaVisAtividade();
     }
 
-    private getAutorias(autorias: Autoria[]): any[] {
-        const quantAutorias = [];
-        const autoriasPorTipo = d3.group(autorias, d => d.tipo_documento);
-        autoriasPorTipo.forEach(autoria => {
-            quantAutorias.push({
-                tipo: autoria[0].tipo_documento,
-                quantAutorias: autoria.length
-            });
-        });
-        return quantAutorias;
-    }
-
     private carregaVisAtividade() {
-        this.atorService.getAutorias(this.idAtor)
+        this.atorService.getAcoes(this.interesse)
         .pipe(takeUntil(this.unsubscribe))
-        .subscribe(autorias => {
-            const dados = this.getAutorias(autorias);
+        .subscribe(acoes => {
             const quantDomain = [];
-            const tipoDomain = [];
-            dados.sort((a, b) => a.quantAutorias - b.quantAutorias);
-            dados.forEach(dado => {
-                tipoDomain.push(dado.tipo);
-                quantDomain.push(dado.quantAutorias);
+            acoes.forEach(dado => {
+                quantDomain.push(dado.num_documentos);
             });
             const maxQuant = Math.max(...quantDomain);
+
             this.gPrincipal = this.svg.append('g')
-                .call(g => this.atualizaVisAtividade(g, tipoDomain, dados, maxQuant, quantDomain));
+                .call(g => this.atualizaVisAtividade(g, acoes, maxQuant));
         });
 
     }
 
-    private atualizaVisAtividade(g, tipoDomain, dados, maxQuant, quantDomain) {
+    private atualizaVisAtividade(g, dados, maxQuant) {
+        const id = this.idAtor;
         const chart = g
             .attr('id', 'chart')
             .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
 
-        this.x.domain([0, maxQuant]);
+        const x = this.x.domain([0, maxQuant]);
 
-        this.y.padding(1).domain(tipoDomain);
+        const y = this.y.padding(1).domain(['Outros', 'Requerimento', 'Emenda']);
 
-        const xAxis = chart.append('g')
+        // Eixo x
+        chart.append('g')
             .attr('class', 'axis axis--x')
             .attr('transform', `translate(0, ${this.altura - this.margin.top - this.margin.bottom})`)
-            .call(d3.axisBottom(this.x).tickValues(quantDomain));
+            .call(d3.axisBottom(x));
 
-        const yAxis = chart.append('g')
+        // Eixo y
+        chart.append('g')
             .attr('class', 'axis axis--y')
-            .call(d3.axisLeft(this.y).tickSize(0))
+            .call(d3.axisLeft(y).tickSize(0))
             .select('.domain').remove();
 
-/*         const title = chart.append('text')
-            .attr('transform', `translate(${(this.largura + this.margin.left + this.margin.right) / 2},20)`)
-            .style('text-anchor', 'middle')
-            .style('font-weight', 700)
-            .text('Atividade por Tipo de Documento'); */
+        const myColor = d3.scaleOrdinal().domain(['Outros', 'Requerimento', 'Emenda'])
+            .range(['#959D97', '#6CA17F', '#4A8D7F']);
 
-        const bars = chart.append('g').attr('transform', `translate(1, -5)`)
+        // Barras - outros atores
+        chart.append('g').attr('transform', `translate(0, -5)`)
             .selectAll('rect').data(dados).join('rect')
             .attr('class', 'bar')
-            .attr('x', 0)
-            .attr('y', (d) => this.y(d.tipo))
-            .attr('width', (d) => this.x(d.quantAutorias))
+            .attr('x', d => x(d.num_documentos))
+            .attr('y', d => y(d.tipo_documento))
+            .attr('width', 2)
             .attr('height', 12)
-            .attr('fill', '#60b27b');
+            .style('fill-opacity', 0.4)
+            .style('fill', d => myColor(d.tipo_documento));
+
+        // Barra - ator atual
+        chart.append('g').attr('transform', `translate(0, -5)`)
+            .selectAll('rect').data(dados.filter(d => d.id_autor_parlametria === id)).join('rect')
+            .attr('class', 'bar')
+            .attr('x', d => x(d.num_documentos))
+            .attr('y', d => y(d.tipo_documento) - 1.5)
+            .attr('width', 4)
+            .attr('height', 15)
+            .style('fill', 'black');
     }
 
 }
