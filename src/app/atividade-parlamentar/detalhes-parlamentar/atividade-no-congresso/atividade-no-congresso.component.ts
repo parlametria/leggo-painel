2e4c66cae87c595e25a4ada857279bb5ca384c12
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-09-22 13:41:37 -0300

    Conserta lint

--- src/app/atividade-parlamentar/detalhes-parlamentar/atividade-no-congresso/atividade-no-congresso.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/atividade-no-congresso/atividade-no-congresso.component.ts
@@ -74,7 +74,7 @@ export class AtividadeNoCongressoComponent implements OnInit {
         this.infoTexto = '';
         const autoriasPorTipo = d3.group(autoriasApresentadas, d => d.tipo_documento);
         autoriasPorTipo.forEach((documento, tipo) => {
-          this.infoTexto += `, ${this.formataPesos(this.somaPesos(documento))} foram ${this.formataTipo(tipo, documento)}`;
+          this.infoTexto += `, ${this.formataPesos(this.somaPesos(documento))} ${this.formataTipo(tipo, documento)}`;
         });
       });
   }
@@ -92,12 +92,23 @@ export class AtividadeNoCongressoComponent implements OnInit {
   }
 
   private formataTipo(tipo, documento): string {
-    let plural = tipo.toLowerCase();
-    if (plural === 'prop. original / apensada') {
-      plural = 'props. originais / apensadas';
+    const peso = this.somaPesos(documento);
+    let tipoFormatado = tipo.toLowerCase();
+    const isPlural = peso > 1;
+
+    if (tipoFormatado === 'prop. original / apensada') {
+      if (isPlural) {
+        tipoFormatado = 'foi proposição original ou apensada';
+      } else {
+        tipoFormatado = 'foram proposições originais ou apensadas';
+      }
     } else {
-      plural = plural + 's';
+      if (isPlural) {
+        tipoFormatado = 'foram ' + tipoFormatado + 's';
+      } else {
+        tipoFormatado = 'foi ' + tipoFormatado;
+      }
     }
-    return plural;
+    return tipoFormatado;
   }
 }
