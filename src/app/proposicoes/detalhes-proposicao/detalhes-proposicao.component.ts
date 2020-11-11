aa82f60522c918c78aedc0cc36e88470aa939598
Andrezj <andre.oliviera@ccc.ufcg.edu.br>
2020-11-11 17:32:05 -0300

    cria componente para exibir detalhes de uma proposicao

---
+++ src/app/proposicoes/detalhes-proposicao/detalhes-proposicao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-proposicao',
+  templateUrl: './detalhes-proposicao.component.html',
+  styleUrls: ['./detalhes-proposicao.component.scss']
+})
+export class DetalhesProposicaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
