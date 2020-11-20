ed881c3f569eebbf869d3e206023c55f059d794a
Joao Menezes <joao.menezes@ccc.ufcg.edu.br>
2020-11-20 10:58:13 -0300

    cria rotas para temperatura e pressão

--- src/app/proposicoes/detalhes-proposicao/detalhes-proposicao-routing.module.ts
+++ src/app/proposicoes/detalhes-proposicao/detalhes-proposicao-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DetalhesProposicaoComponent } from './detalhes-proposicao.component';
+import { TemperaturaPressaoComponent } from './temperatura-pressao/temperatura-pressao.component';
 
 const appRoutes: Routes = [
-    {
+  {
+    path: '',
+    component: DetalhesProposicaoComponent,
+    children: [
+      {
         path: '',
-        component: DetalhesProposicaoComponent,
+        redirectTo: 'temperatura-pressao',
         pathMatch: 'full'
-    }
+      },
+      {
+        path: 'temperatura-pressao',
+        component: TemperaturaPressaoComponent,
+      },
+    ]
+  }
 ];
 
 @NgModule({
