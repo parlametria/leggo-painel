7a1193ed7e86d99c161e58e0bb3833c17911d089
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2020-11-20 16:38:59 -0300

    Adiciona a primeira camada de html e css ao componente Progresso

--- src/app/proposicoes/detalhes-proposicao/progresso/progresso.component.ts
+++ src/app/proposicoes/detalhes-proposicao/progresso/progresso.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+
 import { ActivatedRoute } from '@angular/router';
-import { ProgressoService } from 'src/app/shared/services/progresso.service';
 import { takeUntil, skip } from 'rxjs/operators';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Subject, BehaviorSubject } from 'rxjs';
+
+import { ProgressoService } from 'src/app/shared/services/progresso.service';
 import { ProgressoProposicao } from 'src/app/shared/models/proposicoes/progressoProposicao.model';
+
 import { indicate } from 'src/app/shared/functions/indicate.function';
+import {
+  ordenaProgressoProposicao,
+  resumirFasesProgresso,
+} from 'src/app/shared/functions/process_progresso.function';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-progresso',
@@ -17,6 +26,7 @@ export class ProgressoComponent implements OnInit, OnDestroy {
 
   idLeggo: string;
   progresso: ProgressoProposicao[];
+  fasesResumidas: ProgressoProposicao[];
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -31,25 +41,106 @@ export class ProgressoComponent implements OnInit, OnDestroy {
         if (this.idLeggo !== undefined) {
           this.getProgressoById(this.idLeggo);
         }
-      }
-    );
+      });
+  }
+
+  ordenaProgresso(progresso) {
+    return ordenaProgressoProposicao(progresso);
+  }
+
+  resumirFases(fases) {
+    return resumirFasesProgresso(fases);
   }
 
   getProgressoById(idLeggo: string) {
     this.progressoService
-    .getProgressoProposicaoById(idLeggo)
-    .pipe(
-      indicate(this.isLoading),
-      takeUntil(this.unsubscribe))
-    .subscribe(
-      (progresso) => {
-        this.progresso = progresso;
-        console.log(this.progresso);
-        this.isLoading.next(false);
-      },
-      (error) => {
-        console.log(error);
-      }
+      .getProgressoProposicaoById(idLeggo)
+      .pipe(indicate(this.isLoading), takeUntil(this.unsubscribe))
+      .subscribe(
+        (progresso) => {
+          this.progresso = progresso;
+          this.fasesResumidas = this.resumirFases(
+            this.ordenaProgresso(this.progresso)
+          );
+          console.log(this.fasesResumidas);
+          this.isLoading.next(false);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  isInProgress(fase) {
+    const now = new Date();
+    return (
+      (fase.data_inicio != null && fase.data_fim == null) ||
+      new Date(fase.data_fim) > now
+    );
+  }
+
+  isComissoes(fase) {
+    return fase.local === 'Comissões';
+  }
+
+  isFinished(fase) {
+    const now = new Date();
+    return fase.data_fim != null && new Date(fase.data_fim) < now;
+  }
+
+  isJumpedFase(fase) {
+    return fase.pulou;
+  }
+
+  isFuture(fase) {
+    return (
+      fase.data_fim == null &&
+      fase.data_inicio == null &&
+      !this.isJumpedFase(fase)
+    );
+  }
+
+  formatDate(date) {
+    return moment(date).format('DD/MM/YYYY');
+  }
+
+  formataFase(fase) {
+    if (fase.is_mpv) {
+      return fase.fase_global;
+    }
+
+    return fase.fase_global + ' - ' + fase.local;
+  }
+
+  styleFase(fase) {
+    return {
+      active: this.isFinished(fase),
+      future: this.isFuture(fase),
+      jumped: this.isJumpedFase(fase),
+      inProgress: this.isInProgress(fase),
+      senado:
+        fase.local_casa === 'senado' || fase.fase_global === 'Senado Federal',
+      camara:
+        fase.local_casa === 'camara' ||
+        ['Câmara dos Deputados', 'Câmara dos Deputados - Revisão'].includes(
+          fase.fase_global
+        ),
+      planalto:
+        ['presidência da república', 'congresso'].includes(fase.local_casa) ||
+        ['Sanção Presidencial/Promulgação'].includes(fase.fase_global),
+      'comissao-mista': ['Comissão Mista'].includes(fase.fase_global),
+    };
+  }
+
+  exibeData(data) {
+    return this.formatDate(data) !== 'Invalid date';
+  }
+
+  exibeCasa(fase) {
+    return (
+      !fase.is_mpv &&
+      fase.local_casa !== 'presidência da república' &&
+      fase.local_casa !== 'congresso'
     );
   }
 
