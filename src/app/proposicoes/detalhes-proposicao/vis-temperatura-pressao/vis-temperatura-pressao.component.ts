cfb26e5de0d477bc24c2d4c644ddee55601eed88
Jefferson Neves <jefferson.rpn@gmail.com>
2020-11-26 22:12:48 -0300

    Reposiciona gráficos nos seus devidos espaços

--- src/app/proposicoes/detalhes-proposicao/vis-temperatura-pressao/vis-temperatura-pressao.component.ts
+++ src/app/proposicoes/detalhes-proposicao/vis-temperatura-pressao/vis-temperatura-pressao.component.ts
@@ -56,14 +56,15 @@ export class VisTemperaturaPressaoComponent implements OnInit {
 
   private width;
   private height;
-  private heightG;
+  private heightGrafico;
   private margin;
 
   private x: any;
   private yTemperatura: any;
   private yPressao: any;
   private svg: any;
-  private g: any;
+  private gTemperatura: any;
+  private gPressao: any;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -81,11 +82,11 @@ export class VisTemperaturaPressaoComponent implements OnInit {
     this.width = largura - this.margin.right - this.margin.left;
     this.height = 400 - this.margin.top - this.margin.bottom;
 
-    this.heightG = (this.height * 0.5) - this.margin.bottom;
+    this.heightGrafico = (this.height * 0.5) - this.margin.bottom;
 
     this.x = d3.scaleTime().range([0, this.width]);
-    this.yTemperatura = d3.scaleLinear().range([this.heightG, 0]);
-    this.yPressao = d3.scaleLinear().range([this.heightG, 0]);
+    this.yTemperatura = d3.scaleLinear().range([this.heightGrafico, 0]);
+    this.yPressao = d3.scaleLinear().range([this.heightGrafico, 0]);
 
     this.svg = d3
       .select('#vis-temperatura-pressao')
@@ -96,11 +97,18 @@ export class VisTemperaturaPressaoComponent implements OnInit {
       .attr('viewBox', '0 0 ' +
         (this.width + this.margin.left + this.margin.right) + ' ' + (this.height + this.margin.top + this.margin.bottom));
 
-    this.g = this.svg
+    this.gTemperatura = this.svg
       .append('g')
       .attr(
         'transform',
         'translate(' + this.margin.left + ',' + this.margin.top + ')'
+      );
+
+    this.gPressao = this.svg
+      .append('g')
+      .attr(
+        'transform',
+        'translate(' + this.margin.left + ',' + (this.heightGrafico + this.margin.top * 2) + ')'
       );
 
     this.activatedRoute.parent.paramMap
@@ -131,8 +139,8 @@ export class VisTemperaturaPressaoComponent implements OnInit {
         }));
         temperaturaPressao.map(t => {
           t.data = new Date(t.date),
-          t.valorTemperatura = t.temperatura_recente ?? 0,
-          t.valorPressao = t.trends_max_popularity ?? 0;
+            t.valorTemperatura = t.temperatura_recente ?? 0,
+            t.valorPressao = t.trends_max_popularity ?? 0;
           return t;
         });
       } else {
@@ -142,15 +150,15 @@ export class VisTemperaturaPressaoComponent implements OnInit {
         }));
         temperaturaPressao.map(t => {
           t.data = new Date(t.periodo),
-          t.valorTemperatura = t.temperatura_recente ?? 0,
-          t.valorPressao = t.trends_max_popularity ?? 0;
+            t.valorTemperatura = t.temperatura_recente ?? 0,
+            t.valorPressao = t.trends_max_popularity ?? 0;
           return t;
         });
       }
-      if (this.g) {
-        this.g.selectAll('*').remove();
+      if (this.gTemperatura) {
+        this.gTemperatura.selectAll('*').remove();
       }
-      this.g.call(g => this.atualizarVis(g, temperaturaPressao, temperaturaMax.max_temperatura_periodo));
+      this.gTemperatura.call(g => this.atualizarVis(g, temperaturaPressao, temperaturaMax.max_temperatura_periodo));
     });
   }
 
@@ -158,21 +166,6 @@ export class VisTemperaturaPressaoComponent implements OnInit {
     this.x.domain(d3.extent(dados, (d: any) => d.data));
     this.yTemperatura.domain([0, temperaturaMax]);
     this.yPressao.domain([0, d3.max(dados, (d: any) => d.valorPressao)]);
-
-    this.g.append('g')
-      .attr('transform', `translate(${this.margin.left}, ${this.heightG})`)
-      .call(d3.axisBottom(this.x));
-    this.g.append('g')
-      .attr('transform', `translate(${this.margin.left}, ${this.height - this.margin.bottom})`)
-      .call(d3.axisBottom(this.x));
-    this.g.append('g')
-      .attr('transform', `translate(${this.margin.left}, 0)`)
-      .call(d3.axisLeft(this.yTemperatura).ticks(4));
-    this.g.append('g')
-      .attr('transform', `translate(${this.margin.left}, ${(this.heightG + this.margin.bottom)})`)
-      .call(d3.axisLeft(this.yPressao).ticks(4));
-
-    const color = d3.scaleSequential(this.yTemperatura.domain());
 
     const lineTemperatura = d3.line()
       .curve(d3.curveMonotoneX)
@@ -183,23 +176,36 @@ export class VisTemperaturaPressaoComponent implements OnInit {
       .x((d: any) => this.x(d.data))
       .y((d: any) => this.yPressao(d.valorPressao));
 
-    this.g.append('linearGradient')
+    this.gTemperatura.append('g')
+      .attr('transform', `translate(0, ${this.heightGrafico})`)
+      .call(d3.axisBottom(this.x));
+    this.gTemperatura.append('g')
+      .attr('transform', `translate(0, 0)`)
+      .call(d3.axisLeft(this.yTemperatura).ticks(4));
+    this.gPressao.append('g')
+      .attr('transform', `translate(0, ${this.heightGrafico})`)
+      .call(d3.axisBottom(this.x));
+    this.gPressao.append('g')
+      .attr('transform', `translate(0, 0)`)
+      .call(d3.axisLeft(this.yPressao).ticks(4));
+
+    this.gTemperatura.append('linearGradient')
       .attr('id', 'line-gradient')
       .attr('gradientUnits', 'userSpaceOnUse')
       .attr('x1', 0)
       .attr('y1', 0)
       .attr('x2', 0)
-      .attr('y2', this.heightG)
+      .attr('y2', this.heightGrafico)
       .selectAll('stop')
-        .data([
-          {offset: '0%', color: 'blue'},
-          {offset: '100%', color: 'red'}
-        ])
+      .data([
+        { offset: '0%', color: 'blue' },
+        { offset: '100%', color: 'red' }
+      ])
       .enter().append('stop')
-        .attr('offset', d => d.offset)
-        .attr('stop-color', d => d.color);
+      .attr('offset', d => d.offset)
+      .attr('stop-color', d => d.color);
 
-    this.g.append('path')
+    this.gTemperatura.append('path')
       .datum(dados)
       .attr('fill', 'none')
       .attr('stroke', 'url(\'#line-gradient\'')
@@ -208,7 +214,7 @@ export class VisTemperaturaPressaoComponent implements OnInit {
       .attr('stroke-linecap', 'round')
       .attr('d', lineTemperatura);
 
-    this.g.append('path')
+    this.gPressao.append('path')
       .datum(dados)
       .attr('fill', 'none')
       .attr('stroke', 'url(\'#line-gradient\'')
