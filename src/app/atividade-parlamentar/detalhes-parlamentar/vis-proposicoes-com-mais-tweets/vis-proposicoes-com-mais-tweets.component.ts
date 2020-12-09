09cfb583483b87dd1b2b4b0419e7678e6ed11df0
Jefferson Neves <jefferson.rpn@gmail.com>
2020-12-09 12:01:39 -0300

    Remove console.log

--- src/app/atividade-parlamentar/detalhes-parlamentar/vis-proposicoes-com-mais-tweets/vis-proposicoes-com-mais-tweets.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/vis-proposicoes-com-mais-tweets/vis-proposicoes-com-mais-tweets.component.ts
@@ -38,15 +38,12 @@ export class VisProposicoesComMaisTweetsComponent implements OnInit {
       this.interesse, tema, this.dataInicial, this.dataFinal, this.id, qtd
     ).subscribe(proposicoes => {
       this.proposicaoComMaisTweets = proposicoes;
-      console.log(this.proposicaoComMaisTweets);
-
     });
   }
 
   getNumTweetsNormalizado() {
     this.twitterService.getProposicoesComMaisTweetsPeriodo(this.interesse, this.dataInicial, this.dataFinal).subscribe(
       tweetsPeriodo => {
-        console.log(tweetsPeriodo[0].num_tweets);
         this.maxComentariosPeriodo = tweetsPeriodo[0].num_tweets;
         this.minComentariosPeriodo = tweetsPeriodo.slice(-1)[0].num_tweets;
       }
