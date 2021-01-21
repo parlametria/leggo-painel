3add23de755d806cc29d47afd04fe502be305e17
Hadrizia Santos <hadrizia.santos@ccc.ufcg.edu.br>
2021-01-21 16:04:45 -0300

    Deixa visualização de proposições tweetadas reativa

--- src/app/atividade-parlamentar/detalhes-parlamentar/vis-proposicoes-com-mais-tweets/vis-proposicoes-com-mais-tweets.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/vis-proposicoes-com-mais-tweets/vis-proposicoes-com-mais-tweets.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
+
 import { ProposicaoComMaisTweets } from 'src/app/shared/models/proposicaoComMaisTweets.model';
 import { TwitterService } from 'src/app/shared/services/twitter.service';
 
@@ -24,10 +25,21 @@ export class VisProposicoesComMaisTweetsComponent implements OnInit {
   public minComentariosPeriodo;
 
   constructor(
+    private activatedRoute: ActivatedRoute,
     private twitterService: TwitterService,
   ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams
+    .subscribe(params => {
+      this.tema = params.tema;
+      this.destaque = this.tema === 'destaque';
+      this.tema === undefined || this.destaque ? this.tema = '' : this.tema = this.tema;
+      this.getProposicoes();
+    });
+  }
+
+  getProposicoes() {
     this.twitterService.getProposicoesComMaisTweets(
       this.interesse, this.tema, this.dataInicial, this.dataFinal, this.id, this.qtd, this.destaque)
       .subscribe(proposicoes => {
@@ -38,4 +50,5 @@ export class VisProposicoesComMaisTweetsComponent implements OnInit {
         }, this.proposicoesComMaisTweets[0].num_tweets);
       });
   }
+
 }
