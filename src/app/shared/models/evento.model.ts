9afc641bbebd9ca28b2d9a9e3311ee096b671b4e
Andrezj <andre.oliviera@ccc.ufcg.edu.br>
2021-02-05 18:01:02 -0300

    Cria serviço de consulta a eventos

---
+++ src/app/shared/models/evento.model.ts
@@ -0,0 +1,10 @@
+export interface Evento {
+    data: Date;
+    sigla_local: string;
+    evento: string;
+    titulo_evento: string;
+    texto_tramitacao: string;
+    status: string;
+    proposicao_id: string;
+    nivel: number;
+}
---
+++ src/app/shared/services/eventos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Evento } from '../models/evento.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventosService {
+
+  private eventosUrl = `${environment.baseUrl}/eventos_tramitacao`;
+
+  constructor(private http: HttpClient) { }
+
+  getEventosTramitação(idLeggo: string, interesse: string, dataInicial: string, dataFinal: string): Observable<Evento[]> {
+    const params = new HttpParams()
+      .set('interesse', interesse)
+      .set('data_inicial', dataInicial)
+      .set('data_final', dataFinal)
+      return this.http.get<any>(`${this.eventosUrl}/eventos_tramitacao/${idLeggo}`, { params });
+  }
+}
