aefe203eccdbd134e76e74f43030980f80bb88f4
Jefferson Neves <jefferson.rpn@gmail.com>
2021-03-16 11:48:03 -0300

    Substitui parágrafos e links do corpo do insight por elementos HTML

--- src/app/insights/card-insight/card-insight.component.ts
+++ src/app/insights/card-insight/card-insight.component.ts
@@ -16,4 +16,20 @@ export class CardInsightComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  printInsight(insight) {
+    const linhaRegex = /\r?\n|\r/g;
+    const urlRegex = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g;
+
+    let str = insight.replace(linhaRegex, '<br>');
+
+    const urlMatchs = str.match(urlRegex);
+    if (urlMatchs !== null) {
+      urlMatchs.forEach(match => {
+        str = str.replace(match, `<a href="${match}" target="_blank">${match}</a>`);
+      });
+    }
+
+    return str;
+  }
+
 }
