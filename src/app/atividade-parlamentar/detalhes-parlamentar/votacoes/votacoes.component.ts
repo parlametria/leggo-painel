863a5f4512c9b22a86dbd2bca1adb48cc044763d
João <joao.menezes@ccc.ufcg.edu.br>
2021-03-29 14:43:24 -0300

    lint

--- src/app/atividade-parlamentar/detalhes-parlamentar/votacoes/votacoes.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/votacoes/votacoes.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { Subject, forkJoin } from 'rxjs';
+
+import { VotacoesSumarizadasService } from 'src/app/shared/services/votacoes-sumarizadas.service';
+
+import { AtorService } from 'src/app/shared/services/ator.service';
+import { Ator } from 'src/app/shared/models/ator.model';
+import { VotacoesSumarizadas } from 'src/app/shared/models/votacoesSumarizadas.model';
 
 @Component({
   selector: 'app-votacoes',
@@ -7,9 +16,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotacoesComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe = new Subject();
+
+  public idAtor: string;
+  public interesse: string;
+  public parlamentarInfo: Ator;
+  public votacoesSumarizadas: VotacoesSumarizadas;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private atorService: AtorService,
+    private votacoesSumarizadasService: VotacoesSumarizadasService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.parent.paramMap
+    .pipe(takeUntil(this.unsubscribe))
+    .subscribe(params => {
+      this.idAtor = params.get('id');
+      this.interesse = params.get('interesse');
+      this.resgataVotacoesById(this.idAtor, this.interesse);
+    });
+  }
+
+  private resgataVotacoesById(idAtor, interesse) {
+    forkJoin(
+      [
+        this.atorService.getAtor(interesse, idAtor),
+        this.votacoesSumarizadasService.getVotacoesSumarizadasByID(idAtor)
+      ]
+    )
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(data => {
+        const ator = data[0][0];
+        const votacoes = data[1];
+        this.votacoesSumarizadas = votacoes;
+        this.parlamentarInfo = ator;
+      });
   }
 
 }
---
+++ src/app/shared/models/votacoesSumarizadas.model.ts
@@ -0,0 +1,9 @@
+export interface VotacoesSumarizadas {
+    id_parlamentar: number;
+    id_parlamentar_parlametria: number;
+    casa: string;
+    num_votacoes_parlamentar_disciplina: number;
+    num_votacoes_parlamentar_governismo: number;
+    num_votacoes_totais_disciplina: number;
+    num_votacoes_totais_governismo: number;
+  }
