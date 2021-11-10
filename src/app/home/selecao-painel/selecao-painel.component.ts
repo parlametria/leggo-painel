8c307aa541c262179860d45b46067b3bfe821277
Daniel Henrique <danielhmarinho@gmail.com>
2021-11-09 21:06:22 -0300

    Adding style to cards of home

--- src/app/home/selecao-painel/selecao-painel.component.ts
+++ src/app/home/selecao-painel/selecao-painel.component.ts
@@ -28,6 +28,7 @@ export class SelecaoPainelComponent implements OnInit {
       .getInteresses()
       .subscribe((data) => {
         this.interesses = data.filter((i) => i.interesse !== 'leggo');
+        console.log(this.interesses);
       });
   }
 
