afbd77ed2e687e5fe9efbd178dc39a1245c78f73
Fabio Teixeira <fabio1079@gmail.com>
2022-01-26 18:04:02 -0300

    Remove unused component from adherence folder

--- src/app/aderencia/aderencia.module.ts
+++ src/app/aderencia/aderencia.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FiltroModule } from './filtro/filtro.module';
 
 import { AderenciaComponent } from './aderencia.component';
 import { CongressoChartComponent } from './congresso-chart/congresso-chart.component';
@@ -17,7 +16,6 @@ import { AderenciaSwitchBlockComponent } from './aderencia-switch-block/aderenci
   imports: [
     CommonModule,
     NgbModule,
-    FiltroModule,
   ]
 })
 export class AderenciaModule { }
