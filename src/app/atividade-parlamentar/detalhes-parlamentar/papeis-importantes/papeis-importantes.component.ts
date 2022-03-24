8e5f2c136cb660cfb73517d73707bac5bcb4b365
ArthurJahn <stutrzbecher@gmail.com>
2022-03-24 16:10:09 -0300

    adiciona select de interesses na aba de trajetória

--- src/app/atividade-parlamentar/detalhes-parlamentar/papeis-importantes/papeis-importantes.component.ts
+++ src/app/atividade-parlamentar/detalhes-parlamentar/papeis-importantes/papeis-importantes.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Subject, BehaviorSubject, forkJoin } from 'rxjs';
 import { takeUntil, skip } from 'rxjs/operators';
 
+import { InteresseService } from 'src/app/shared/services/interesse.service';
+import { Interesse } from 'src/app/shared/models/interesse.model';
 import { ComissoesCargo } from 'src/app/shared/models/comissaoPresidencia.model';
 import { Relatorias } from 'src/app/shared/models/atorRelator.model';
 import { Autoria } from 'src/app/shared/models/autoria.model';
@@ -11,11 +13,18 @@ import { ComissaoService } from 'src/app/shared/services/comissao.service';
 import { RelatoriaService } from 'src/app/shared/services/relatoria.service';
 import { AutoriasService } from 'src/app/shared/services/autorias.service';
 
+const DEFAULT_INTERESSE: Interesse = {
+  nome_interesse: 'Todos os painéis',
+  descricao_interesse: '',
+  interesse: 'todos'
+};
+
 @Component({
   selector: 'app-papeis-importantes',
   templateUrl: './papeis-importantes.component.html',
   styleUrls: ['./papeis-importantes.component.scss']
 })
+
 export class PapeisImportantesComponent implements OnInit {
 
   private unsubscribe = new Subject();
@@ -28,11 +37,15 @@ export class PapeisImportantesComponent implements OnInit {
   public isLoading = new BehaviorSubject<boolean>(true);
   public tema: string;
   public destaque: boolean;
+  public interesses: Interesse[] = [DEFAULT_INTERESSE];
+  public selectedInteresse: Interesse = DEFAULT_INTERESSE;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private comissaoService: ComissaoService,
     private relatoriaService: RelatoriaService,
+    private interesseService: InteresseService,
+    private router: Router,
     private autoriasService: AutoriasService
   ) { }
 
@@ -49,6 +62,10 @@ export class PapeisImportantesComponent implements OnInit {
         this.destaque = this.tema === 'destaque';
         this.tema === undefined || this.destaque ? this.tema = '' : this.tema = this.tema;
         this.getParlamentarDetalhado(this.idAtor, this.interesse, this.tema, this.destaque);
+
+        if (this.interesses.length === 1) { // apenas o DEFAULT
+          this.resgataInteresses();
+        }
       });
   }
 
@@ -66,6 +83,46 @@ export class PapeisImportantesComponent implements OnInit {
       this.autorias = data[2];
       this.isLoading.next(false);
     });
+  }
+
+  private resgataInteresses() {
+    this.interesseService.getInteresses()
+      .subscribe((data) => {
+        const interesses = data.filter((i) => i.interesse !== 'leggo');
+        this.interesses = [DEFAULT_INTERESSE, ...interesses];
+
+        if (this.interesse === undefined) {
+          this.selectedInteresse = DEFAULT_INTERESSE;
+        } else {
+          const found = this.interesses.find(i => i.interesse === this.interesse);
+          this.selectedInteresse = found ?? DEFAULT_INTERESSE;
+        }
+      });
+  }
+
+  interesseSelecionado(value: string) {
+    const interesse = this.interesses.find(i => i.interesse === value);
+
+    if (interesse === undefined) {
+      console.log('interesse not found');
+      return;
+    }
+
+    this.selectedInteresse = interesse;
+
+    const queryParams: Params = Object.assign({}, this.activatedRoute.snapshot.queryParams);
+
+    if (this.selectedInteresse.interesse !== DEFAULT_INTERESSE.interesse) {
+      queryParams.interesse = this.selectedInteresse.interesse;
+    } else {
+      queryParams.interesse = undefined;
+    }
+
+    const { scrollX, scrollY } = window;
+    this.router.navigate([], { queryParams })
+      .then(() => {
+        window.scrollTo(scrollX, scrollY);
+      });
   }
 
 }
