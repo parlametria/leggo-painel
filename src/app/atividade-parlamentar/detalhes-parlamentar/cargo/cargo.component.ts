8de0b3e4b6405dcd772e5d425a87752db51d762b
ArthurJahn <stutrzbecher@gmail.com>
2022-03-24 20:22:29 -0300

    adiciona aba de trajetória

---
+++ src/app/atividade-parlamentar/detalhes-parlamentar/cargo/cargo.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
+
+import { getClassCargo } from '../../../shared/functions/comissao';
+
+@Component({
+  selector: 'app-cargo',
+  templateUrl: './cargo.component.html',
+  styleUrls: ['./cargo.component.scss']
+})
+export class CargoComponent implements OnChanges {
+
+  @Input() cargos: {};
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      typeof changes.cargos !== 'undefined' &&
+      typeof changes.cargos.currentValue !== 'undefined'
+    ) {
+      Object.keys(this.cargos).map(cargo => { this.cargos[cargo].classe = getClassCargo(cargo); });
+    }
+  }
+
+}
