76871a5a1ad66039656ec20f488c2cf16a7e21f1
ArthurJahn <stutrzbecher@gmail.com>
2022-09-08 19:14:48 -0300

    remove reqs ao twitter antigo

--- src/app/shared/services/twitter.service.ts
+++ src/app/shared/services/twitter.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { AtorTwitter } from '../models/atorTwitter.model';
@@ -23,28 +24,33 @@ export class TwitterService {
   constructor(private http: HttpClient) { }
 
   getTweets(parlamentar: ParlamentarPerfil): Observable<Embed[]> {
-    return this.http.get<Embed[]>(`${this.twitter}?parlamentar=${parlamentar.entidade}`);
+    return of([])
+    //return this.http.get<Embed[]>(`${this.twitter}?parlamentar=${parlamentar.entidade}`);
   }
 
   getTweetsInteresse(parlamentar: ParlamentarPerfil): Observable<InteresseTweet[]> {
-    return this.http.get<InteresseTweet[]>(`${this.twitter}${parlamentar.entidade}/`);
+    return of([])
+    //return this.http.get<InteresseTweet[]>(`${this.twitter}${parlamentar.entidade}/`);
   }
 
-  getTweetsInfo(): Observable<InfoTweets> {
-    return this.http.get<InfoTweets>(this.twitterInfoUrl);
+  getTweetsInfo(): Observable<any> {
+    return of(null)
+    //return this.http.get<InfoTweets>(this.twitterInfoUrl);
   }
 
-  getAtividade(idEntidade: string): Observable<ParlamentarPerfil> {
+  getAtividade(idEntidade: string): Observable<any> {
     if (!idEntidade) {
       return;
     }
     const path = `${this.parlamentarUrl}${idEntidade}/`;
-    return this.http.get<ParlamentarPerfil>(path);
+    return of(null)
+    //return this.http.get<ParlamentarPerfil>(path);
   }
 
   getEngajamentoParlamentar(parlamentar: ParlamentarPerfil): Observable<Engajamento[]> {
     const path = `${this.engajamentoUrl}${parlamentar.entidade}/`;
-    return this.http.get<Engajamento[]>(path);
+    return of([])
+    //return this.http.get<Engajamento[]>(path);
   }
 
 
@@ -56,7 +62,8 @@ export class TwitterService {
       .set('data_inicial', dataInicial)
       .set('data_final', dataFinal)
       .set('destaque', destaque.toString());
-    return this.http.get<any[]>(`${this.twitterUrl}/tweets/parlamentares`, { params });
+    return of([])
+    //return this.http.get<any[]>(`${this.twitterUrl}/tweets/parlamentares`, { params });
   }
 
   getAtividadeDetalhadaTwitter(
@@ -68,21 +75,24 @@ export class TwitterService {
       .set('data_inicial', dataInicial)
       .set('data_final', dataFinal)
       .set('destaque', destaque.toString());
-    return this.http.get<any>(`${this.twitterUrl}/tweets/parlamentares/${id}`, { params });
+    return of(null)
+    //return this.http.get<any>(`${this.twitterUrl}/tweets/parlamentares/${id}`, { params });
   }
 
   getMediaTweets(dataInicial: string, dataFinal: string): Observable<any> {
     const params = new HttpParams()
       .set('data_inicial', dataInicial)
       .set('data_final', dataFinal);
-    return this.http.get<any>(`${this.twitterUrl}/parlamentares/media`, { params });
+    return of(null)
+    //return this.http.get<any>(`${this.twitterUrl}/parlamentares/media`, { params });
   }
 
   getEngajamento(dataInicial: string, dataFinal: string): Observable<any> {
     const params = new HttpParams()
       .set('data_inicial', dataInicial)
       .set('data_final', dataFinal);
-    return this.http.get<any>(`${this.twitterUrl}/parlamentares/engajamento`, { params });
+    return of(null)
+    //return this.http.get<any>(`${this.twitterUrl}/parlamentares/engajamento`, { params });
   }
 
   getProposicoesComMaisTweets(
@@ -95,7 +105,8 @@ export class TwitterService {
       .set('data_final', dataFinal)
       .set('qtd', qtd)
       .set('destaque', destaque.toString());
-    return this.http.get<any>(`${this.twitterUrl}/proposicoes/parlamentar/${id}`, { params });
+    return of([])
+    //return this.http.get<any>(`${this.twitterUrl}/proposicoes/parlamentar/${id}`, { params });
   }
 
   getProposicoesComMaisTweetsPeriodo(interesse: string, dataInicial: string, dataFinal: string): Observable<ProposicaoComMaisTweets[]> {
@@ -103,11 +114,13 @@ export class TwitterService {
       .set('interesse', interesse)
       .set('data_inicial', dataInicial)
       .set('data_final', dataFinal);
-    return this.http.get<any>(`${this.twitterUrl}/proposicoes/mais-comentadas`, { params });
+    return of([])
+    //return this.http.get<any>(`${this.twitterUrl}/proposicoes/mais-comentadas`, { params });
   }
 
-  getUsernameTwitter(id: string): Observable<AtorTwitter> {
-    return this.http.get<any>(`${this.twitterUrl}/parlamentares/username/${id}`);
+  getUsernameTwitter(id: string): Observable<any> {
+    return of(null)
+    //return this.http.get<any>(`${this.twitterUrl}/parlamentares/username/${id}`);
   }
 
   getTweetsParlamentar(
@@ -127,14 +140,17 @@ export class TwitterService {
       params = params.set('limit', limit.toString());
     }
 
-    return this.http.get<Tweet[]>(`${this.twitterUrl}/tweets/${id}/texto`, { params });
+    return of([]);
+    //return this.http.get<Tweet[]>(`${this.twitterUrl}/tweets/${id}/texto`, { params });
   }
 
-  getInfoTwitter(): Observable<InfoTwitter> {
-    return this.http.get<InfoTwitter>(`${this.twitterUrl}/tweets/info`);
+  getInfoTwitter(): Observable<any> {
+    return of(null);
+    //return this.http.get<InfoTwitter>(`${this.twitterUrl}/tweets/info`);
   }
 
-  getTweetRawInfo(): Observable<InfoTwitter> {
-    return this.http.get<InfoTwitter>(`${this.twitterUrl}/tweets/info/raw`);
+  getTweetRawInfo(): Observable<any> {
+    return of(null);
+    //return this.http.get<InfoTwitter>(`${this.twitterUrl}/tweets/info/raw`);
   }
 }
